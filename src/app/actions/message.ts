d4b6199b5a30de5b35abe65c3e0464d771e64cbe
aabboudi <139150604+aabboudi@users.noreply.github.com>
2026-03-17 22:25:51 +0000

    fix: add temp safe parsing to user input

--- src/app/actions/message.ts
+++ src/app/actions/message.ts
@@ -2,14 +2,19 @@
 
 import { sendEmail } from "@/lib/email";
 import { FormData } from "../new/form";
+import { escape } from "he";
 
 export async function sendNotification(data: FormData) {
   const to = process.env.CONTACT_MESSAGE_RECEPTION!.split(",")
+  const safeName = escape(data.name)
+  const safeEmail = escape(data.email)
+  const safeMessage = escape(data.message)
+
   return await sendEmail({
     to: to,
     subject: `New message`,
-    emailHtml: `<p><strong>From:</strong> ${data.name} &lt;${data.email}&gt;</p><p>${data.message.replace(/\n/g, "<br />")}</p>`,
-    emailText: `From: ${data.name} <${data.email}>\n\n${data.message}`,
+    emailHtml: `<p><strong>From:</strong> ${safeName} &lt;${safeEmail}&gt;</p><p>${safeMessage.replace(/\n/g, "<br />")}</p>`,
+    emailText: `From: ${safeName} <${safeEmail}>\n\n${safeMessage}`,
   })
 }
 
